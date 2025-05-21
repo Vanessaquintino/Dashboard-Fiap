07d269836ccb1062199eb6b38f15f7d754549597
Vanessa Quintino <56452821+Vanessaquintino@users.noreply.github.com>
2025-05-21 14:30:44 -0300

    Create Sobre.py

---
+++ pages/Sobre.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+# Página de sobre
+st.title('Contatos 📞')
+st.markdown('''
+Olá! Somos [Luciana May](https://www.linkedin.com/in/luciana-may-a602a5164/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app) e [Vanessa Quintino](https://www.linkedin.com/in/vanessa-quintino-9352b513b/)
+
+Atualmente, cursamos pós-graduação em Data Analytics na FIAP. Neste trimestre, enfrentamos o desafio de analisar grandes volumes de dados disponibilizados pelo site da U.S. Energy Information Administration (EIA).
+
+Nosso objetivo foi extrair:
+1. Insights relevantes;
+2. Testar modelos preditivos;
+3. Reunir informações que possam apoiar a tomada de decisões estratégicas.
+
+O projeto envolveu desde a coleta e tratamento dos dados até a construção de dashboards interativos e modelos de previsão, utilizando ferramentas como Python, Streamlit, Databricks e XGBoost. Esperamos que este trabalho contribua para uma melhor compreensão das dinâmicas do mercado de petróleo e auxilie na tomada de decisões baseadas em dados.
+''')
